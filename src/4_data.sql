0670916b58d6d23452248369d3e613197a4b3c81
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-10-27 12:19:59 +0100

    Add SQL

---
+++ src/4_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO products_categories(name, description)
+VALUES ('Mursten', ''),
+       ('Tagsten', '')
+
+EXEC InsertProduct 'Lille sten', '', 'Mursten', 100000;
+EXEC InsertProduct 'Sten', '', 'Mursten', 150000;
+EXEC InsertProduct 'Stor sten', '', 'Mursten', 200000;
+EXEC InsertProduct 'Lille tagsten', '', 'Tagsten', 200000;
+EXEC InsertProduct 'Tagsten', '', 'Tagsten', 250000;
+EXEC InsertProduct 'Stor tagsten', '', 'Tagsten', 300000;
