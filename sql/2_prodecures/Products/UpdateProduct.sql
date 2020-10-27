b5021da5e3cf7046bf496a0df270a20082a466d4
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-10-27 14:43:02 +0100

    fix formatting

--- sql/2_prodecures/Products/UpdateProduct.sql
+++ sql/2_prodecures/Products/UpdateProduct.sql
@@ -1,16 +1,16 @@
 CREATE PROCEDURE UpdateProduct
-@Id INT,
-@Name NVARCHAR(255),
-@Description NVARCHAR(MAX),
-@CategoryName NVARCHAR(255),
-@Price INT
+    @Id INT,
+    @Name NVARCHAR(255),
+    @Description NVARCHAR(MAX),
+    @CategoryName NVARCHAR(255),
+    @Price INT
 AS
-UPDATE products
-SET name = @Name,
-    description = @Description,
-    categoryId = (SELECT id FROM products_categories WHERE name = @CategoryName)
-WHERE id = @Id;
-DECLARE @CurrentPrice INT = (SELECT price FROM products_prices WHERE productId = @Id AND endTime > GETUTCDATE());
-IF @CurrentPrice != @Price
+    UPDATE products
+    SET name = @Name,
+        description = @Description,
+        categoryId = (SELECT id FROM products_categories WHERE name = @CategoryName)
+    WHERE id = @Id;
+    DECLARE @CurrentPrice INT = (SELECT price FROM products_prices WHERE productId = @Id AND endTime > GETUTCDATE());
+    IF @CurrentPrice != @Price
     EXEC AddPriceToProduct @Id, @Price
 ;
--- src/database/IProductDB.java
+++ src/database/IProductDB.java
@@ -7,14 +7,9 @@ import java.util.List;
 
 public interface IProductDB {
     List<Product> findAll();
-
     Product findById(int id) throws DataAccessException;
-
     List<Product> findByName(String name);
-
     List<Product> findByCategoryName(String name);
-
     Product create(String name, String description, String categoryName, int price);
-
     void update(int id, String name, String description, String categoryName, int price);
 }
