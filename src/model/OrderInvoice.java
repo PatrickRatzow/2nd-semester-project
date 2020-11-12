923e762ee5318c911524e9113d7778122c23e360
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-11-12 03:04:20 +0100

    Add OrderInvoice class

---
+++ src/model/OrderInvoice.java
@@ -0,0 +1,29 @@
+package model;
+
+public class OrderInvoice {
+    private Product product;
+    private int quantity;
+
+    public OrderInvoice() {}
+
+    public OrderInvoice(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
