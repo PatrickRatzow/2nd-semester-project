520e11d514198cae81fd8d3487016f77ab6f920e
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-11-12 02:40:16 +0100

    Refactor Project DAO pattern

---
+++ src/database/ProjectDao.java
@@ -0,0 +1,5 @@
+package database;
+
+interface ProjectDao {
+
+}
--- src/database/ProjectDaoMsSql.java
+++ src/database/ProjectDaoMsSql.java
@@ -1,47 +1,39 @@
 package database;
 
+import model.Project;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import model.Project;
-
-public class ProjectDB {
-
-	
-	private static final String GET_ALL_PROJECTS_Q= "";
+public class ProjectDaoMsSql implements ProjectDao {
+	private static final String GET_ALL_PROJECTS_Q = "";
 	private PreparedStatement getAllProjectsPS;
-	
 	private static final String INSERT_PERSON_TO_PROJECT_Q = "";
 	private PreparedStatement insertPersonToProjectPS;
-	
 	private static final String GET_PERSON_ON_PROJECT_Q = "";
 	private PreparedStatement getPersonOnProjectPS;
-	
 	private static final String INSER_Q = "";
 	private PreparedStatement insertPS;
-	
 	private static final String UPDATE_Q = "";
 	private PreparedStatement updatePS;
-	
 	private static final String DELETE_Q = "";
 	private PreparedStatement deletePS;
 	
-	
-	public ProjectDB() {
+	public ProjectDaoMsSql() {
 		init();
 	}
 	
 	private void init() {
-		DBConnection con = DBConnection.getInstance();
+		final DBConnection con = DBConnection.getInstance();
+
 		try {
 			getAllProjectsPS = con.prepareStatement(GET_ALL_PROJECTS_Q);
 
 		} catch(SQLException e) {
-			
+			e.printStackTrace();
 		}
-		
 	}
 	
 	public Project buildObject(ResultSet rs) {
@@ -51,5 +43,4 @@ public class ProjectDB {
 	public List<Project> buildObjects(ResultSet rs) {
 		return null;
 	}
-	
 }
