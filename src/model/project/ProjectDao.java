436c2bb24373a355f9e9e2048b13d88cefd1d555
SleepyC4 <nikolajcarlslund@gmail.com>
2020-11-12 16:52:14 +0100

    Project

--- src/model/project/ProjectDao.java
+++ src/model/project/ProjectDao.java
@@ -1,5 +1,27 @@
 package model.project;
 
+
+import java.util.List;
+
+import exception.DataAccessException;
+import model.Person;
+
 public interface ProjectDao {
 
+	public List<Project> findAll() throws DataAccessException;
+	
+	public void findProjectByCustomer();
+	
+	public Person findPersonOnProject(Person p);
+	
+	public void addPersonToProject();
+	
+	public void create();
+	
+	public void update();
+	
+	public void delete();
+	
+	
+	
 }
