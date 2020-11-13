c4e486fcf1875d79060c759791eecf6692843c9a
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-11-13 15:23:09 +0100

    Fix wrong parameter for OrderService

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -3,9 +3,9 @@ package service;
 import exception.DataAccessException;
 import exception.DataWriteException;
 import model.Order;
-import model.OrderLine;
+import model.Project;
 
 public interface OrderService {
     Order findById(int id) throws DataAccessException;
-    Order create(Order order, OrderLine orderLine) throws DataWriteException;
+    Order create(Order order, Project project) throws DataWriteException;
 }
