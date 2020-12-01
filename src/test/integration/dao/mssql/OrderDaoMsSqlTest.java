348119729c15055389eb400d73ab7038314932e3
SleepyC4 <nikolajcarlslund@gmail.com>
2020-12-01 11:46:15 +0100

    OrderDao SQL

--- src/test/integration/dao/mssql/OrderDaoMsSqlTest.java
+++ src/test/integration/dao/mssql/OrderDaoMsSqlTest.java
@@ -5,12 +5,17 @@ import dao.mssql.OrderDaoMsSql;
 import datasource.DBConnection;
 import datasource.DBManager;
 import entity.Order;
+import entity.OrderStatus;
 import exception.DataAccessException;
+import exception.DataWriteException;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
 
 public class OrderDaoMsSqlTest {
     private static DBConnection connection;
@@ -32,6 +37,21 @@ public class OrderDaoMsSqlTest {
 
         // Assert
         assertNotNull(order);
+    }
+    
+    
+    @Test
+    void create() throws DataWriteException {
+    	//Arrange
+
+    	Order order = null;
+    	
+    	//Act
+    	
+    	order = dao.create(LocalDateTime.now(), OrderStatus.AWAITING, 1, 1, 1);
+    	
+    	//Assert
+    	assertNotNull(order);
     }
 
     @AfterAll
