1de77ff81643a540592c2f2629dbc1c9d2d0f22f
SleepyC4 <nikolajcarlslund@gmail.com>
2020-12-03 21:37:21 +0100

    Update Customers.java

--- src/gui/Customers.java
+++ src/gui/Customers.java
@@ -1,4 +1,13 @@
 package gui;
 
-public class Customers {
+import gui.components.core.TabPanel;
+
+public class Customers extends TabPanel{
+	
+	//Do not use for now it gives error
+	public Customers() {
+		setActive("customer", () -> new AddCustomer()); 
+		//-- should probably be made to another Customer class;
+	}
+	
 }
