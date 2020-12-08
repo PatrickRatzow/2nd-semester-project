e142c0ccf9c5e4e51e9d903d5839b1c644bc36f0
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-12-08 11:27:15 +0100

    fix: adds missing files

---
+++ src/gui/tabs/product_finder/ProductFinderResult.java
@@ -0,0 +1,25 @@
+package gui.tabs.product_finder;
+
+import controller.OrderController;
+import gui.components.core.PanelManager;
+import gui.components.core.TitleBar;
+import gui.components.product.ProductsBox;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProductFinderResult extends JPanel {
+	public ProductFinderResult(PanelManager panelManager, OrderController orderController) {
+		String previousId = panelManager.getCurrentId();
+		setLayout(new BorderLayout(0, 0));
+		
+		TitleBar titleBar = new TitleBar();
+		titleBar.setTitle("Resultat");
+		titleBar.setButtonName("Gå Tilbage");
+		titleBar.addActionListener(l -> panelManager.setActiveAndRemoveCurrent(previousId));
+		add(titleBar, BorderLayout.NORTH);
+
+		ProductsBox box = new ProductsBox(orderController.getOrderLines(), orderController.getPrice());
+		add(box, BorderLayout.CENTER);
+	}
+}
---
+++ src/gui/tabs/project/ProjectView.java
@@ -0,0 +1,24 @@
+package gui.tabs.project;
+
+import controller.ProjectController;
+import gui.components.core.PanelManager;
+import gui.components.core.TitleBar;
+import gui.components.project.ProjectOverview;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProjectView extends JPanel {
+	public ProjectView(PanelManager panelManager, ProjectController projectController) {
+		setLayout(new BorderLayout(0, 0));
+
+		TitleBar titleBar = new TitleBar();
+		titleBar.setTitle(projectController.getName());
+		titleBar.setButtonName("Gå Tilbage");
+		titleBar.addActionListener(l -> panelManager.setActiveAndRemoveCurrent(panelManager.getPreviousId()));
+		add(titleBar, BorderLayout.NORTH);
+		
+		ProjectOverview overview = new ProjectOverview(projectController);
+		add(overview, BorderLayout.CENTER);
+	}
+}
