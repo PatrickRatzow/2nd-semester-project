1646f4038cec150d56f00bcc26905414b4237422
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-12-16 03:14:07 +0100

    fix: fixes tests being dead

--- sql/2_views/0_products/0_view_product.sql
+++ sql/2_views/0_products/0_view_product.sql
@@ -7,7 +7,7 @@ SELECT
     pp.start_time AS price_start_time,
     pp.end_time AS price_end_time,
     pf.field_id AS field_id,
-    pf.value AS field_value
+    pf.field_value AS field_value
 FROM product p
 INNER JOIN product_price pp on p.id = pp.product_id
 LEFT JOIN product_field pf on p.id = pf.product_id
