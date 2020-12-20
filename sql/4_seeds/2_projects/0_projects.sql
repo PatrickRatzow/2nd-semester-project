8dcd3e4947617720edf3610ef9b5f426bb6adf6e
SleepyC4 <nikolajcarlslund@gmail.com>
2020-12-20 02:16:33 +0100

    Slettede test data

--- sql/4_seeds/2_projects/0_projects.sql
+++ sql/4_seeds/2_projects/0_projects.sql
@@ -1,5 +1,4 @@
 INSERT INTO project(name, status, customer_id, employee_id, price, estimated_hours)
 VALUES ('The Project', 3, 1, 1, 200000, 24),
-       ('The Minker',  3, 2, 1, 980000, 78),
        ('Hus #1', 1, 2, 2, 10000, 7),
        ('Hus #2', 2, 1, 2, 90000, 63)
