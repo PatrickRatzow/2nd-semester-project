c40b5e88ae7ba7118ea2cc86b4331c2d27bfd149
SleepyC4 <nikolajcarlslund@gmail.com>
2020-12-20 02:20:34 +0100

    Opdaterede values

--- sql/4_seeds/2_projects/0_projects.sql
+++ sql/4_seeds/2_projects/0_projects.sql
@@ -1,4 +1,5 @@
 INSERT INTO project(name, status, customer_id, employee_id, price, estimated_hours)
-VALUES ('The Project', 3, 1, 1, 200000, 24),
+VALUES ('Skur #1', 3, 1, 1, 200000, 24),
+       ('Tag #1',  3, 2, 1, 980000, 78),
        ('Hus #1', 1, 2, 2, 10000, 7),
        ('Hus #2', 2, 1, 2, 90000, 63)
